ec82830587227101cb36fa039eed94a3f1de456e
Blake <32065552+ITDallasDBW@users.noreply.github.com>
2025-12-19 12:17:49 -0600

    4.15 Sum of Two complete. Interviews section Loop 1 complete

--- questions/sumOfTwo/index.js
+++ questions/sumOfTwo/index.js
@@ -11,20 +11,54 @@
  * 
  */
 
+console.log("4.15 Sum of Two")
+//twoSum used hashmaps. This uses sets
+
+//naive way = nested for loops. 
+//Add each number in nums1 to each in nums2
+//Time=n^2. Boo
+
+//Better:
+//Start with target-nums1[0]=difference
+//Need to find x iff nums2[x]===difference
+//Need to store difference temporarily
+//Temp array inefficient b/c time
+//Temp array hashmaop would need key. Boo
+//So use SET to store difference
+//Set= think of as hashmap without key. Just a 'set' of UNIQUE values (or as array w/o dups)
+//establish set = {nums1[0]-target=set[0], nums1[1]-target=set[1], etc}
+//Then, can loop through set against nums2 checking for === w/set.has
+
  const sumOfTwo = (nums1, nums2, target) => {
-   const set = new Set();
+  const set=new Set()
 
-   for (num of nums1) {
-     set.add(target - num);
-   }
+  for (num of nums1) {
+    set.add(target-num)
+  }
 
-   for (num of nums2) {
-     if (set.has(num)) {
-       return true;
-     }
-   }
+  for (num of nums2){
+    if (set.has(num)){
+      return true
+    }
+  }
 
-   return false;
+  return false;
+  
  };
 
 module.exports = sumOfTwo;
+
+
+  //  const set = new Set();
+
+  //  for (num of nums1) {
+  //    set.add(target - num);
+  //  }
+
+  //  for (num of nums2) {
+  //    if (set.has(num)) {
+  //      return true;
+  //    }
+  //  }
+
+  //  return false;
